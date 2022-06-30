12e06354f9710eac92fe209ddb4ffcd198024564
Malcolm Keyes <3.malcolmk@gmail.com>
2022-06-30 08:09:47 -0400

    commit tree 5e8185a7d2a475720aa60e2f1cc9141753c7bdf7

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true, length: {minimum: 1}
+    validates :category, presence: true, inclusion: { in: %w(Fiction Non-Fiction)}
+    validates :content, presence: true, length: {minimum: 100}
+    
 end
